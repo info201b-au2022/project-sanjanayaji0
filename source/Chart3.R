a4556b990a2cd5b45963c16b48d9e08b59bab885
sanjanayaji0 <sanjana.yaji0@gmail.com>
2022-11-29 02:37:40 -0800

    final changes

--- source/Chart3.R
+++ source/Chart3.R
@@ -7,14 +7,22 @@ library('plotly')
 
 #Recieving Data
 hdi <- read.csv("https://hdr.undp.org/sites/default/files/2021-22_HDR/HDR21-22_Composite_indices_complete_time_series.csv")
-View(hdi)
 
 energyGridComp <- read.csv("https://nyc3.digitaloceanspaces.com/owid-public/data/energy/owid-energy-data.csv")
-View(energyGridComp)
+
+energyGridComp <- energyGridComp %>%
+  rename(biofuel = biofuel_share_elec, 
+         coal = coal_share_elec, 
+         gas = gas_share_elec, 
+         hydro = hydro_share_elec,
+         carbon = low_carbon_share_elec, 
+         oil = oil_share_elec,
+         nuclear = nuclear_share_elec, 
+         other = other_renewables_share_elec)
 
 # Cleaning up data
 energyGrid <- energyGridComp %>%
-  select(country, year, biofuel_share_elec, coal_share_elec, gas_share_elec, hydro_share_elec, low_carbon_share_elec, oil_share_elec, nuclear_share_elec, other_renewables_share_elec) %>% 
+  select(country, year, biofuel, coal, gas, hydro, carbon, oil, nuclear, other) %>% 
   group_by(country) %>%
   filter(year == "2021") %>%
   drop_na() %>%
@@ -27,7 +35,9 @@ energyGrid$Largest_Share<-colnames(energyGrid)[apply(energyGrid,1,which.max)]
 hdi2021 <- hdi %>%
   select(country, hdi_2021)
 
-energyGrid <- energyGrid %>% select(-c(biofuel_share_elec, coal_share_elec, gas_share_elec, hydro_share_elec, low_carbon_share_elec, oil_share_elec, nuclear_share_elec, other_renewables_share_elec))
+energyGrid <- energyGrid %>% select(-c(biofuel, coal, gas, hydro, 
+                                       carbon, oil, nuclear, other))
+
 
 View(energyGrid)
 View(hdi2021)
@@ -36,9 +46,12 @@ energyByhdi <- merge(energyGrid, hdi2021, by="country")
 
 #Plotting graph
 chart3 <- ggplot(energyByhdi) +
-  geom_point(mapping = aes(x = Largest_Share, y = hdi_2021, color = country)) 
+  geom_point(mapping = aes(x = Largest_Share, y = hdi_2021, color = country)) + 
+  theme(plot.margin = margin(1, 1, 2, 2, "cm"))
 
 chart3 <- ggplotly(chart3)
+
+
 
 
   
